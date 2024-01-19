4174d2901f31cd9189ffa130f17c66cf1abafba5
qfurumo <furumq@gmail.com>
2024-01-19 12:32:44 -0500

    Added the test data to a directory that test_coverage() can access, and changed tests file accordingly

--- tests/testthat/test-PIPETS_FullRun.R
+++ tests/testthat/test-PIPETS_FullRun.R
@@ -1,13 +1,13 @@
 test_that("Test that input parameters are acceptable", {
-    expect_no_error(PIPETS_FullRun(inputData = "inst/extdata/PIPETS_TestData.bed", readLength = 58, OutputFileID = "Test"))
+    expect_no_error(PIPETS_FullRun(inputData = "PIPETS_TestData.bed", readLength = 58, OutputFileID = "Test"))
     expect_warning(PIPETS_FullRun(inputData = NA,readLength = 58, OutputFileID = "Test"))
-    expect_warning(PIPETS_FullRun(inputData = "inst/extdata/PIPETS_TestData.bed",readLength = NA,OutputFileID = "Test"))
+    expect_warning(PIPETS_FullRun(inputData = "PIPETS_TestData.bed",readLength = NA,OutputFileID = "Test"))
 
-    expect_warning(PIPETS_FullRun(inputData = "inst/extdata/PIPETS_TestData.bed",readLength = 0,OutputFileID = "Test"))
-    expect_warning(PIPETS_FullRun(inputData = "inst/extdata/PIPETS_TestData.bed",readLength = 58,slidingWindowSize = 0,OutputFileID = "Test"))
+    expect_warning(PIPETS_FullRun(inputData = "PIPETS_TestData.bed",readLength = 0,OutputFileID = "Test"))
+    expect_warning(PIPETS_FullRun(inputData = "PIPETS_TestData.bed",readLength = 58,slidingWindowSize = 0,OutputFileID = "Test"))
 
-    expect_warning(PIPETS_FullRun(inputData = "inst/extdata/PIPETS_TestData.bed",readLength = 58,slidingWindowSize = NA,OutputFileID = "Test"))
-    expect_warning(PIPETS_FullRun(inputData = "inst/extdata/PIPETS_TestData.bed",readLength = 58,topEndPercentage = 0,OutputFileID = "Test"))
+    expect_warning(PIPETS_FullRun(inputData = "PIPETS_TestData.bed",readLength = 58,slidingWindowSize = NA,OutputFileID = "Test"))
+    expect_warning(PIPETS_FullRun(inputData = "PIPETS_TestData.bed",readLength = 58,topEndPercentage = 0,OutputFileID = "Test"))
 })
 
 
